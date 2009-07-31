b03266cfe3cef67198e9f892c8a37c2e3134a791
Abdel A Saleh <asaleh@the-experimenters.com>
2009-07-31 23:35:21 +0100

    perkit based on virtual player, - Initial version of perkit communicating to a virtual downloader and mp3 player (based on quicktime). - Added several docs regarding thoughts on download speed and some components.

---
+++ hardware/perkit_control/applet/perkit_control.cpp
@@ -0,0 +1,121 @@
+/*
+  Perkit - Podcast Enabled Radio Kit
+  Language: Wiring/Arduino (pin numbers defined for Arduino)
+*/
+
+#define stopped 0
+#define downloading 1
+#define downloaded 2
+#define playerInitiated 3
+#define playing 4
+
+#define stateLED 13
+
+#define onOffSwitch 2
+
+#include "WProgram.h"
+void setup();
+void loop();
+void performRadioFunctions();
+void setLED();
+void blink(int led);
+int status = stopped;
+int rxByte= -1;
+
+int ledBlinkState = 0;
+long timeOfLastBlink = 0;
+int blinkInterval = 200;
+
+void setup(){
+  pinMode(stateLED, OUTPUT);
+  pinMode(onOffSwitch, INPUT);
+  
+  Serial.begin(9600);
+}
+
+void loop(){
+  Serial.println(status);
+  performRadioFunctions();
+  setLED();
+}
+
+void performRadioFunctions(){
+  switch(status){
+    case stopped:
+      if(digitalRead(onOffSwitch) == HIGH) 
+      {
+        Serial.println("D");
+        status = downloading;
+      }
+      break;
+    case downloading:
+      if(digitalRead(onOffSwitch) == LOW) 
+      {
+        Serial.println("S");
+        status = stopped;
+      }
+      else
+      {
+        if(Serial.available())
+        {
+          rxByte = Serial.read();
+          if (rxByte == 'C') status = downloaded;
+        }
+      }
+      break; 
+    case downloaded:
+      Serial.println("I");
+      status = playerInitiated;
+      break;
+    case playerInitiated:
+      if(Serial.available())
+      {
+        rxByte = Serial.read();
+        if (rxByte == 'P') status = playing;
+      }
+      break;
+    case playing:
+      break;
+  }
+}
+
+void setLED(){
+  switch(status){
+    case stopped:
+      digitalWrite(stateLED, LOW);
+      break;
+    case downloading:
+      blink(stateLED);
+      break; 
+    case downloaded:
+      blink(stateLED);
+      break;
+    case playerInitiated:
+      blink(stateLED);
+      break;
+    case playing:
+      digitalWrite(stateLED, HIGH);
+  }
+}
+
+void blink(int led){
+  if(millis() - timeOfLastBlink > blinkInterval)
+  {
+    timeOfLastBlink = millis();
+    ledBlinkState = (ledBlinkState == LOW ? HIGH : LOW);
+    digitalWrite(led, ledBlinkState);
+  }
+}
+
+int main(void)
+{
+	init();
+
+	setup();
+    
+	for (;;)
+		loop();
+        
+	return 0;
+}
+
